65bd35e9d1c61fe01bcab1a3c405ef2a3f2e0225
Stephane Eybert <mittiprovence@yahoo.se>
2018-10-17 14:43:22 +0200

    Renamed the deployment script.

--- deploy-ng-zero.sh
+++ deploy-ng-zero.sh
@@ -6,3 +6,10 @@ rm -fr ~/dev/github/pages/stephaneeybert/ng-zero/*
 
 cp -R ~/dev/js/projects/angular/ng-zero/dist/* ./ng-zero/
 
+git add ng-zero/
+
+git commit -m "Deployed the ng-zero application the `date +%Y-%m-%d` at `date +%H-%M`"
+
+git pull
+
+git push
