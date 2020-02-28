1dfe28554c289c2a7b6869c0641d80ffa2a3227c
Stephane Eybert <mittiprovence@yahoo.se>
2020-02-28 14:01:20 +0100

    Renamed the application.

---
+++ deploy-musicng.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+
+# Building the project for the Git Pages project
+# ng build --prod --base-href /stephaneeybert/musicng/
+
+cd ~/dev/github/pages/stephaneeybert
+git rm -r musicng/*
+mkdir musicng
+cp -R ~/dev/js/projects/angular/musicng/dist/* ./musicng/
+
+git add musicng/
+git commit -m "Deployed the musicng application the `date +%Y-%m-%d` at `date +%H:%M`"
+git pull
+git push
