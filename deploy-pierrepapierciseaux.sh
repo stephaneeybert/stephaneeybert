3fbc30305a7329773f33fe6a9a4eddadbf47f3e2
Stephane Eybert <mittiprovence@yahoo.se>
2020-03-01 17:47:15 +0100

    Added a script to deploy another application.

---
+++ deploy-pierrepapierciseaux.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+# Building the project for the Git Pages project
+# ng build --prod --base-href /stephaneeybert/pierrepapierciseaux/
+# cp dist/index.html dist/404.html
+
+cd ~/dev/github/pages/stephaneeybert
+git rm -r pierrepapierciseaux/*
+mkdir pierrepapierciseaux
+cp -R ~/dev/js/projects/angular/pierrepapierciseaux/dist/* ./pierrepapierciseaux/
+
+git add pierrepapierciseaux/
+git commit -m "Deployed the pierrepapierciseaux application the `date +%Y-%m-%d` at `date +%H:%M`"
+git pull
+git push
+
+# Open in the browser at https://stephaneeybert.github.io/stephaneeybert/pierrepapierciseaux/
