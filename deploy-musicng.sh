86229c63cd6ed469d1497e24d967c22a64cffb36
Stephane Eybert <mittiprovence@yahoo.se>
2020-03-01 10:44:09 +0100

    Updated the deployment instructions.

--- deploy-musicng.sh
+++ deploy-musicng.sh
@@ -2,11 +2,12 @@
 
 # Building the project for the Git Pages project
 # ng build --prod --base-href /stephaneeybert/musicng/
+# cp  dist/index.html dist/404.html
 
 cd ~/dev/github/pages/stephaneeybert
 git rm -r musicng/*
 mkdir musicng
-cp -R ~/dev/js/projects/angular/musicng/dist/* ./musicng/
+cp -R ~/dev/js/projects/angular/musicng/deploy/* ./musicng/
 
 git add musicng/
 git commit -m "Deployed the musicng application the `date +%Y-%m-%d` at `date +%H:%M`"
